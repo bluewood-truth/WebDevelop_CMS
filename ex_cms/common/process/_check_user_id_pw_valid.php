0964199d081d1ea5c3eb6073818dcffe7e199df5
bluewood-truth <55024033+bluewood-truth@users.noreply.github.com>
2020-06-13 00:00:24 +0900

    Update 200612

--- ex_cms/common/process/_check_user_id_pw_valid.php
+++
@@ -1,6 +0,0 @@
-<?php
-include $_SERVER["DOCUMENT_ROOT"]."/ex_cms/common/common.php";
-sql_connect();
-
-echo sql_login_check($_POST["id"],$_POST["pw"]) ? 'true' : 'false';
- ?>
--- ex_cms/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once $_SERVER["DOCUMENT_ROOT"]."/ex_cms/common/common.php";
-sql_connect();
-
-$sql = "SELECT * FROM CMS_userinfo WHERE id=4";
-$result = sql_query($sql);
-$row = sql_get_row($result);
-echo $row["is_admin"];
- ?>
